9ff6e45795db7c56f697b4ff081619317870e2e2
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2025-11-03 00:02:41 +0200

    N queens problem instance generator

---
+++ core/search_strategies/n_queens_problem/n_queens_instance_generator.py
@@ -0,0 +1,40 @@
+import random
+from .n_queens_validator import NQueensValidator
+
+class NQueensInstanceGenerator:
+    """Generates a random N-Queens instance given board size and number of queens."""
+
+    @staticmethod
+    def generate(board_size: int, num_queens: int) -> dict:
+        if num_queens > board_size:
+            raise ValueError("Number of queens cannot exceed board size")
+
+        board = [[0] * board_size for _ in range(board_size)]
+        validator = NQueensValidator()
+
+        attempts = 0
+        while True:
+            attempts += 1
+            # Reset board
+            for r in range(board_size):
+                for c in range(board_size):
+                    board[r][c] = 0
+
+            # Place queens randomly
+            positions = random.sample(range(board_size), num_queens)
+            for r, c in enumerate(positions):
+                board[r][c] = 1
+
+            # Validate board
+            if validator.is_valid(board, num_queens):
+                break
+
+            if attempts > 100:
+                raise RuntimeError("Failed to generate a valid N-Queens instance after 100 tries")
+
+        return {
+            "problem_name": "N-Queens",
+            "board_size": board_size,
+            "queen_number_on_board": num_queens,
+            "board": board
+        }
